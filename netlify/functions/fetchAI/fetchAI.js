d10bc178763d6199e8c9c86f69f3acd659ed4d24
peng.zhao <pengzhao2000jp@yahoo.co.jp>
2023-06-07 16:32:19 +0900

    client update 4

--- netlify/functions/fetchAI/fetchAI.js
+++ netlify/functions/fetchAI/fetchAI.js
@@ -4,7 +4,7 @@ const handler = async (event) => {
     const subject = event.queryStringParameters.name || 'World'
     return {
       statusCode: 200,
-      body: event.body
+      body: JSON.stringify(event.body)
     }
   } catch (error) {
     return { statusCode: 500, body: error.toString() }
