65d1adc41e26cb843a4e4ffc828b5cda573083d3
peng.zhao <pengzhao2000jp@yahoo.co.jp>
2023-06-07 16:28:21 +0900

    client update 2

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
-import { Configuration, OpenAIApi } from 'openai'
-
-const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
-})
-
-
-const openai = new OpenAIApi(configuration)
-
 const chatbotConversation = document.getElementById('chatbot-conversation')
-
 
 const conversationArr = [
     {
