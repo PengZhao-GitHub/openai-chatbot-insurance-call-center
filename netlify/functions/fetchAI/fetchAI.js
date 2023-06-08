30c03cc33904b0cd8c16118f156812a7c8440fc5
peng.zhao <pengzhao2000jp@yahoo.co.jp>
2023-06-08 12:12:47 +0900

    2023.06.08

--- netlify/functions/fetchAI/fetchAI.js
+++ netlify/functions/fetchAI/fetchAI.js
@@ -6,7 +6,8 @@ const handler = async (event) => {
     const requestBody = event.body;
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: `${requestBody}` }),
+      //body: JSON.stringify({ message: `${requestBody}` }),
+      body: JSON.stringify({ message: requestBody }),
     }
   } catch (error) {
     return { statusCode: 500, body: error.toString() }
