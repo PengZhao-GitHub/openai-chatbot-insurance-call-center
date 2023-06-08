158c575bf8b603b952b652b3d239ec48b1466558
peng.zhao <pengzhao2000jp@yahoo.co.jp>
2023-06-08 10:39:26 +0900

    2023.06.08

--- netlify/functions/fetchAI/fetchAI.js
+++ netlify/functions/fetchAI/fetchAI.js
@@ -5,7 +5,7 @@ const handler = async (event) => {
 
   try {
     //const subject = event.queryStringParameters.name || 'World'
-    const requestBody = JSON.parse(event.body)
+    const requestBody = event.body
     return {
       statusCode: 200,
       body: JSON.stringify({
