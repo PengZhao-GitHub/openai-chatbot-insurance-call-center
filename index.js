a05b59b9a49d30377633d2beef569da87fc61353
peng.zhao <pengzhao2000jp@yahoo.co.jp>
2023-06-08 10:49:17 +0900

    2023.06.08

--- index.js
+++ index.js
@@ -50,10 +50,12 @@ async function fetchReply() {
     })
 
     console.log(response)
+    console.log(response.body)
+    console.log(JSON.parse(response.body))
 
-    const data = await response.json()
+    //const data = await response.json()
 
-    console.log(JSON.parse(data))
+   //console.log(JSON.parse(data))
 
     // Add the AI response to DOM
     //renderTypewriterText(response.data.choices[0].message.content)
